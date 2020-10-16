96c1cf446ec18a0e8433c1f2e0813452e09acccd
Brijkishor24 <71825478+Brijkishor24@users.noreply.github.com>
2020-10-16 13:57:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 //https://teachablemachine.withgoogle.com/models/pje28U0Dy/
+Webcam.set({
+    height:300,
+    width:350,
+    image_format:"png",
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+function take_photo() {
+    Webcam.snap(function(data_url){
+        document.getElementById("result").innerHTML="<img id='captured_image' src='"+data_url+"'>";
+    });
+}
+console.log(ml5.version);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/pje28U0Dy/model.json",modelloaded);
+function modelloaded(){
+    console.log("Model loaded");
+}
+
